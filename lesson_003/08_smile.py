b42feef1c5af70eb5752134c566cd63cff833b6b
Илья Растригин <ilyrast@gmail.com>
2021-02-05 13:44:29 +0300

    1, 2, 3, 4, 8 с правками

--- lesson_003/08_smile.py
+++ lesson_003/08_smile.py
@@ -2,7 +2,9 @@
 
 # (определение функций)
 import simple_draw as sd
+
 sd.resolution = (1200, 600)
+
 
 # Написать функцию отрисовки смайлика по заданным координатам
 # Форма рожицы-смайлика на ваше усмотрение
@@ -12,23 +14,27 @@ sd.resolution = (1200, 600)
 
 # TODO, пожалуйста, реализуйте функцию Смайлик =)
 
-for _ in range(10):
-    x = sd.random_number(0, 1200)
-    y = sd.random_number(0, 600)
-    point_main = sd.get_point(x, y)
+def smile(x_coord, y_coord, color):
+    point_main = sd.get_point(x_coord, y_coord)
     radius_main = 50
-    point_1 = sd.get_point(x-20, y+20)
+    point_1 = sd.get_point(x_coord - 20, y_coord + 20)
     radius_1 = 5
-    point_2 = sd.get_point(x+20, y+20)
-    color = sd.COLOR_WHITE
-    p1 = sd.get_point(x - 25, y - 20)
-    p2 = sd.get_point(x - 10, y - 25)
-    p3 = sd.get_point(x + 10, y - 25)
-    p4 = sd.get_point(x + 25, y - 20)
+    point_2 = sd.get_point(x_coord + 20, y_coord + 20)
+    p1 = sd.get_point(x_coord - 25, y_coord - 20)
+    p2 = sd.get_point(x_coord - 10, y_coord - 25)
+    p3 = sd.get_point(x_coord + 10, y_coord - 25)
+    p4 = sd.get_point(x_coord + 25, y_coord - 20)
     lines = p1, p2, p3, p4
     sd.circle(center_position=point_main, radius=radius_main, color=color)
     sd.circle(center_position=point_1, radius=radius_1, color=color)
     sd.circle(center_position=point_2, radius=radius_1, color=color)
     sd.lines(point_list=lines, color=color, closed=False, width=1)
+
+
+for _ in range(10):
+    x_coord = sd.random_number(0, 1200)
+    y_coord = sd.random_number(0, 600)
+    color = sd.COLOR_WHITE
+    smile(x_coord, y_coord, color=color)
 
 sd.pause()
