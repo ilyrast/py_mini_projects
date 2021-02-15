3d34d853698d45a8e8361993fa3550acda70c48c
13gin <ilyrast@gmail.com>
2021-02-15 22:29:52 +0300

    4.1 готов 4.2 готов 4.3 готов

--- lesson_004/03_shape_select.py
+++ lesson_004/03_shape_select.py
@@ -2,6 +2,38 @@
 
 import simple_draw as sd
 
+def triangle(point, angle, length):
+    new_point = point
+    for angle in range(angle + 0, angle + 361, 120):
+        v = sd.get_vector(start_point=new_point, angle=angle, length=length, width=3)
+        v.draw()
+        new_point = v.end_point
+
+
+def square(point, angle, length):
+    new_point = point
+    for angle in range(angle + 0, angle + 361, 90):
+        v = sd.get_vector(start_point=new_point, angle=angle, length=length, width=3)
+        v.draw()
+        new_point = v.end_point
+
+
+def pentagon(point, angle, length):
+    new_point = point
+    for angle in range(angle + 0, angle + 361, 72):
+        v = sd.get_vector(start_point=new_point, angle=angle, length=length, width=3)
+        v.draw()
+        new_point = v.end_point
+
+
+def hexagon(point, angle, length):
+    new_point = point
+    for angle in range(angle + 0, angle + 361, 60):
+        v = sd.get_vector(start_point=new_point, angle=angle, length=length, width=3)
+        v.draw()
+        new_point = v.end_point
+
+
 # Запросить у пользователя желаемую фигуру посредством выбора из существующих
 #   вывести список всех фигур с номерами и ждать ввода номера желаемой фигуры.
 # и нарисовать эту фигуру в центре экрана
@@ -9,6 +41,23 @@ import simple_draw as sd
 # Код функций из упр lesson_004/02_global_color.py скопировать сюда
 # Результат решения см lesson_004/results/exercise_03_shape_select.jpg
 
-# TODO здесь ваш код
+print("Возможные фигуры:")
+
+figure = {0: "Треугольник", 1: "Квадрат", 2: "Пятиугольник", 3: "Шестиугольник"}
+
+figure_list = (triangle, square, pentagon, hexagon)
+
+for key, value in figure.items():
+    print(key, ':', value)
+
+number = input()
+number = int(number)
+
+if number > 3 or number < 0:
+    print('Неверный номер')
+
+length = 100
+middle_point = sd.get_point(sd.resolution[0]/2 - length/2, sd.resolution[1]/2 - length/2)
+figure_list[number](point=middle_point, angle=0, length=length)
 
 sd.pause()
