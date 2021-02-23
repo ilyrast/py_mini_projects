e944b0643db07b99c55438c9cb032431133752d9
13gin <ilyrast@gmail.com>
2021-02-23 21:50:37 +0300

    5 готово

--- lesson_004/05_snowfall.py
+++ lesson_004/05_snowfall.py
@@ -15,46 +15,31 @@ N = 20
 # sd.sleep()
 # sd.random_number()
 # sd.user_want_exit()
-snow_list = []
-len_compare = []
 
-for i in range(N):
-    x = sd.random_number(10, sd.resolution[0])
-    len_i = sd.random_number(10, 100)
-    # TODO переменная len_compare и цикл получились лишними =)
-    while len_compare.count(len_i) == True:
-        len_i = sd.random_number(10, 100)
-    len_compare.append(len_i)
-    snow_list.append([x, sd.resolution[1] - 10, len_i])
 
-while True:
-    sd.clear_screen()
-    # TODO в цикле по списку c enumerate правильно так
-    #  for index, element in ...
-    for l_list in enumerate(snow_list):
-        # TODO, переменная id лишняя =)
-        id, coord_x, coord_y, length = l_list[0], *l_list[1]
-        print(id, coord_x, coord_y, length)
-        point = sd.get_point(coord_x, coord_y)
-        sd.snowflake(center=point, length=length)
-        if coord_y < 10:
-            coord_y = sd.resolution[1] -10
-        snow_list[id] = ([coord_x, coord_y - 30, length])
-    sd.sleep(0.1)
-    if sd.user_want_exit():
-        break
-
-# Примерный алгоритм отрисовки снежинок
-#   навсегда
-#     очистка экрана
-#     для индекс, координата_х из списка координат снежинок
-#       получить координата_у по индексу
-#       изменить координата_у и запомнить её в списке по индексу
-#       создать точку отрисовки снежинки по координатам
-#       нарисовать снежинку белым цветом в этой точке
-#     немного поспать
-#     если пользователь хочет выйти
-#       прервать цикл
+# snow_list = []
+# #len_compare = []
+#
+# for i in range(N):
+#     x = sd.random_number(10, sd.resolution[0])
+#     len_i = sd.random_number(10, 100)
+#     # TODO переменная len_compare и цикл получились лишними =)
+#     # почему лишними? если убрать цикл, то не все длины будут раные
+#    #n_compare.append(len_i)
+#     snow_list.append([x, sd.resolution[1] - 10, len_i])
+#
+# while True:
+#     sd.clear_screen()
+#     for index, l_list in enumerate(snow_list):
+#         coord_x, coord_y, length = l_list
+#         point = sd.get_point(coord_x, coord_y)
+#         sd.snowflake(center=point, length=length)
+#         if coord_y < 10:
+#             coord_y = sd.resolution[1] - 10
+#         snow_list[index] = ([coord_x, coord_y - 30, length])
+#     sd.sleep(0.1)
+#     if sd.user_want_exit():
+#         break
 
 
 # Часть 2 (делается после зачета первой части)
@@ -85,8 +70,43 @@ while True:
 #       прервать цикл
 
 
+snow_list = []
+
+for i in range(N):
+    x = sd.random_number(10, sd.resolution[0])
+    y = sd.random_number(50, sd.resolution[1])
+    len_i = sd.random_number(10, 35)
+    snow_list.append([x, y, len_i])
+
+while True:
+    sd.start_drawing()
+    for index, l_list in enumerate(snow_list):
+        coord_x, coord_y, length = l_list
+        point = sd.get_point(coord_x, coord_y)
+        sd.snowflake(center=point, length=length, color=sd.background_color)
+        if coord_y < 10:
+            sd.snowflake(center=point, length=length)
+            snow_list.pop(index)
+            snow_list.append([
+                sd.random_number(10, sd.resolution[0]),
+                sd.random_number(sd.resolution[1] - 50, sd.resolution[1]),
+                sd.random_number(10, 35)
+            ])
+        else:
+            deviation = sd.random_number(-20, 20)
+            snow_list[index] = ([coord_x + deviation, coord_y - 10, length])
+            point_1 = sd.get_point(coord_x + deviation, coord_y - 10)
+            sd.snowflake(center=point_1, length=length)
+    sd.finish_drawing()
+    sd.sleep(0.1)
+    if sd.user_want_exit():
+        break
+
+
+
+
 # Усложненное задание (делать по желанию)
 # - сделать рандомные отклонения вправо/влево при каждом шаге
-# - сделать сугоб внизу экрана - если снежинка долетает до низа, оставлять её там,
+# - сделать сугроб внизу экрана - если снежинка долетает до низа, оставлять её там,
 #   и добавлять новую снежинку
 # Результат решения см https://youtu.be/XBx0JtxHiLg
