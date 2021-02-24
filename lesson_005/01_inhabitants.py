beceb726b1fc7590e62905312ce8e3205cbe2b84
13gin <ilyrast@gmail.com>
2021-02-24 17:33:35 +0300

    1,2

--- lesson_005/01_inhabitants.py
+++ lesson_005/01_inhabitants.py
@@ -3,4 +3,10 @@
 # Вывести на консоль жителей комнат (модули room_1 и room_2)
 # Формат: В комнате room_1 живут: ...
 
-# TODO здесь ваш код
+from room_1 import folks
+from room_2 import folks as f1
+
+print('В комнате room_1 живут:', ', '.join(folks))
+print('В комнате room_2 живёт', ''.join(f1))
+
+
--- lesson_005/02_district.py
+++ lesson_005/02_district.py
@@ -5,4 +5,18 @@
 # подсказка: для вывода элементов списка через запятую можно использовать функцию строки .join()
 # https://docs.python.org/3/library/stdtypes.html#str.join
 
-# TODO здесь ваш код
+from district.central_street.house1.room1 import folks as f0
+from district.central_street.house1.room2 import folks as f1
+from district.central_street.house2.room1 import folks as f2
+from district.central_street.house2.room2 import folks as f3
+s = ', '
+name_list = []
+for index, name in enumerate(f0):
+    name_list.append(name)
+for index, name in enumerate(f1):
+    name_list.append(name)
+for index, name in enumerate(f2):
+    name_list.append(name)
+for index, name in enumerate(f3):
+    name_list.append(name)
+print('На районе живут:', s.join(name_list))
