caa1dd4bc936d744e2e439f78a0d582835c1be08
Insolente13 <lanc05@mail.ru>
2021-02-25 19:17:31 +0300

    Lesson 05 02,04 - TODO, 03 - ok

--- lesson_005/02_district.py
+++ lesson_005/02_district.py
@@ -9,6 +9,9 @@ from district.central_street.house1.room1 import folks as f0
 from district.central_street.house1.room2 import folks as f1
 from district.central_street.house2.room1 import folks as f2
 from district.central_street.house2.room2 import folks as f3
+
+# TODO, исходя из задания, из папки soviet_street импорты тоже необходимы =)
+
 s = ', '
 name_list = []
 
